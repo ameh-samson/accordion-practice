1dff7e9d1029b3d560eae44fce5b153ffe9a71d2
Samson Ameh <amehsam69@gmail.com>
2024-04-17 19:23:22 +0100

    add state

--- src/Question.jsx
+++ src/Question.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
+
 const Question = ({ title, info }) => {
+  const [isVisible, setIsVisible] = useState(false);
   return (
     <article className="question">
       <header>
         <h4>{title}</h4>
-        <button className="btn">+</button>
+        <button className="btn" onClick={() => setIsVisible(!isVisible)}>
+          {!isVisible ? "+" : "-"}
+        </button>
       </header>
-      <p>{info}</p>
+      {isVisible ? <p>{info}</p> : null}
     </article>
   );
 };
